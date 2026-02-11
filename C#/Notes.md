b917e80e08399805a18221344f739d2c934a04bd
ali-osman-de <demirkolluosmanali@gmail.com>
2026-02-11 21:25:35 +0300

    Temeller-26-36

---
+++ C#/Notes.md
@@ -0,0 +1,20 @@
+### C# Orta Seviyeli Dillerden (Yüksek - Konuşma dili --> Düşük - Makina Dili)
+- Microsoft Tarafından Open-Source olarak sunulmaktadır.
+
+### .NET Framework vs .NET Core
+- .NET bir geliştirme çatısıdır.
+- Framework ve Core her türlü platform için geliştirilebilir fakat framework windows sunucu özelinde, core ise tüm makinalara deploy edelebilir. Kısaca framework windows'a bağlı core ise cross-platform diyebiliyoruz.
+
+### Compiler Nedir? ve Nasıl Çalışır?
+- Derlemek yazılan dilin bilgisayarın anlayacağı dile çevrilmesidir. (Örnek olarak .exe dosyaları bilgisayarın anlayacağı dille çevrilmiş dosyalardandır.)
+
+### Project vs Solution 
+- Solution: İçerisinde bir veya birden fazla projeyi kapsayan bütünlük.
+- Project: Kodlarının yazdığın bir bütün parçası.
+
+### Temel Kod Prensipleri
+- Kendi Tekrar Etmeme, anlık çözüm oluşturma, ezberleme yok.
+- Anlamlı isimlendirme.
+
+
+
---
+++ C#/Temeller-26-36/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Top-Level-Statements Nedir?
+// Hazır Gelen Main ve class'ı yazmak zorunda bırakılmaması, fakat gizli bir yazılan kodu kavrayan bir Main olduğunu unutmamak lazım.
+internal class Program
+{
+    #region Main Nedir?
+    // Program.cs içerisinde Main fonksiyonu oluşturulan tüm uygulamalardaki başlangıç noktası
+    // .cs => C#
+    // Uygulama ayağa kalktığında işletim sistemi ile iletişim kuran fonksiyon
+    // en fazla 1 adet olmalıdır
+    private static void Main(string[] args)
+    {
+        /*
+            dotnet run ali
+            output => "Hello, World!"
+                       ali
+        */
+        // Burada args aslında console bir dinleme yapıyor fakat dotnet run [(),(),()] gibi
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(args[0]);
+
+        /*
+            dotnet run ali osman
+            output => "Hello, World!"
+                       ali osman
+        */
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine($"{args[0]} {args[1]}");
+    }
+    #endregion
+
+}
+#endregion
