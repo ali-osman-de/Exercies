32cc0151dbeadcb8f3ba7c8a209078661ea6b335
ali-osman-de <demirkolluosmanali@gmail.com>
2026-02-12 14:40:32 +0300

    Temeller-71-91

---
+++ C#/Temeller-71-91/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFromConsole = "";
+        inputFromConsole =  Console.ReadLine();
+        Console.WriteLine(inputFromConsole);
+        
+        #region Senkron vs Asenkron
+            /*
+                Senkronda bir gişeye sıra mantığı vardır. önceki kişinin işi bitmeden bir sonrakine geçilmez
+                Asenkronda birden fazla gişeye bir sıra var gibi düşünebiliriz. burada ise öncekini beklemeden açık olan gişeye geçebilir.
+            
+            */
+        #endregion
+
+
+
+    }
+}
