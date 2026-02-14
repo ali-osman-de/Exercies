931a9071f5dd5ca411c6c0331b32b6aa1897093f
ali-osman-de <demirkolluosmanali@gmail.com>
2026-02-14 16:40:07 +0300

    Temeller-334-402

--- C#/Temeller-334-402/Program.cs
+++ C#/Temeller-334-402/Program.cs
@@ -1,7 +1,33 @@
-﻿public class Program
+﻿using System.Text;
+
+public class Program
 {
     static void Main(string[] args)
     {
+        #region ArraySegment ve StringSegment
+            //ArraySegment bir orijinal diziden referans almadan o dizi üzerinde işlem yapılmasıdır. Yani ekstra bir ref oluşturmadan işlem yapabilmek.
+            int[] diziSegment = {1,2,3,4,5,6};
+            ArraySegment<int> segment1 = ArraySegment<int>(diziSegment); // Burada aslında dizisegmenti referansına ref attık.
+            ArraySegment<int> slice = segment1.Slice(0,3);
+            ArraySegment<int> segment2 = ArraySegment<int>(diziSegment, 2, 3); // 2.indexten  3 kadar daha gidecek
+            Console.WriteLine(segment2);
+
+            // StringSegment --> Microsoft.Extensions.Primitive paketten yüklenmesi gerekiyor.
+
+            string stringSegment = "Gelecek geçmişten bir parça içerir!";
+            // stringSegment segment3 = new(stringSegment, 3, 5); 
+        #endregion
         
+        #region StringBuilder Sınıfı
+            // performanslı string segment alg. kullanarak textleri birleştirip döndürür.
+            string name = "ali";
+            string lname = "osman";
+
+            StringBuilder builder = new StringBuilder();
+            builder.Append(name);
+            builder.Append(lname);
+
+            Console.WriteLine(builder.ToString());
+        #endregion
     }
 }
