19c940fadac43e055cc47e357af7e2c6e2f4a92d
ali-osman-de <demirkolluosmanali@gmail.com>
2026-02-15 20:44:50 +0300

    OOP_1_Class

--- C#/OOP-1/OOP-1.md
+++ C#/OOP-1/OOP-1.md
@@ -9,3 +9,11 @@
 - Bir class modelinden birden fazla nesne üretebiliriz.
 - Nesneler hangi türdendir? --> Referans türlüdür.
 - Direkt nesnelere erişim sağlamıyoruz. Bir referans türlü değişken tanımlıyoruz --> bu değişken bir nesneye refere ediyor. Yani ref. değişken Stack'te, Nesne ise Heap'te.
+
+
+### Class Kavramı - Sınıf Kavramı - Sınıf Nesne Arasındaki ilişki
+- Bir obje modellenmesi gerekir, yani bir obje bir Class nesnesine denk gelir.
+- Sınıf ile Nesne arasındaki ilişki --> Class'tan birçok nesne oluşturulabilir.
+- Class'lar ayrıca tür özelliği de taşır.
+- Namespace içerisinde, dışında, Class içerisinde(Nested type) oluşturulabilirler.
+
---
+++ C#/OOP-1/OOP_1_Class.cs
@@ -0,0 +1,45 @@
+﻿namespace OOP_Dersleri
+{
+    public class OOP_1_Class
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+        }
+    }  
+
+    // Class içerisinde class oluşturma (Nested Type)
+    // aynı namespace içerisindeki classlar birbirine direkt erişebilirken
+    class NestedClass
+    {
+        
+    };
+
+    //Referans alma/noktası alma
+    //Şu anda bir herhangi bir nesne ataması yapılmamış hali
+    NesneModeli nesneModeli;
+}
+
+// Classlar Namespace dışında da oluşturulabilir
+// farklı namespacedeki classlar ise namespace üzerinden birbirlerine erişir.  örnek:  namespace.x
+// aşağıdaki gibi namespace dışında ise direkt erişim sağlarız.
+class NameSpaceClass
+{
+    
+} 
+
+// Sınıf ile Nesne modeli nasıl oluşturulur? 
+class NesneModeli
+{
+    // Field, Metot oluşturulabilir.
+    // Değişkenler eğer bir class içerisinde ise onlara Field diyoruz.
+    int a;
+    int b;
+
+    public int Topla()
+    {
+        return a+b;
+    }
+}
