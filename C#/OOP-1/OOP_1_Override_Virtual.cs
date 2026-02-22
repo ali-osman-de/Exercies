b6e1c40d7307e9332f1820a26226db800a04ab3d
ali-osman-de <demirkolluosmanali@gmail.com>
2026-02-23 01:17:40 +0300

    OOP_1_Override_Virtual

--- C#/OOP-1/OOP_1_Override_Virtual.cs
+++ C#/OOP-1/OOP_1_Override_Virtual.cs
@@ -2,6 +2,37 @@ class OOP_1_Override_Virtual
 {
     static void Main(string[] args)
     {
-        
+        // Virtual yapılandırmalar run-time sırasında hangi sınıftan türediği bildirilir.
+        // Virtual --> yapılar metot veya property olabilir
+        // Name-hiden farklı olarak virtual yapılar bildirilme esasına dayalı iken name-hiden olayında yani aynı isimde farklı prop veya metotların başka sınıf aynı isimde tanımlanması ile birlikte birlikte artık o sınıftaki isimle kullanılmasıdır.
+        // Virtual metotlar veya proplar geç bağlanma yani LATE BINDING ile bağlanır.
+
+        MyClass myClass = new MyClass();
+        MyClass2 myClass2 = new MyClass2();
+        myClass.Topla(1,2);
+        myClass2.Topla(1,2);
+    }
+    class MyClass
+    {
+        public virtual int Topla(int a, int b)
+        {
+            return a+b;
+        }
+    }
+
+    class MyClass2 : MyClass
+    {
+        public override int Topla(int a, int b)
+        {
+            return a-b;
+        }
+    }
+    
+    class MyClass3 : MyClass
+    {
+        public override int Topla(int a, int b)
+        {
+            return a % 2 + b;
+        }
     }
 }
